f1b1aab8e9850167355fd4451668c33ccb9f2a63
ac1ra <glombinski@gmail.com>
2026-01-13 15:52:02 +0300

    update APIViews

--- delivsite/woman/views.py
+++ delivsite/woman/views.py
@@ -1,64 +1,46 @@
 # from django.shortcuts import render
 from django.forms import model_to_dict
-from rest_framework import generics
-
+from rest_framework import generics,viewsets,mixins
+from rest_framework.decorators import action
 # from .serializer import WomanSerializer
+from rest_framework.viewsets import GenericViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
-from .models import Woman
+from .models import Woman, Category
 from .serializer import WomanSerializer
 
-class WomanAPIList(generics.ListCreateAPIView):
-    queryset = Woman.objects.all()
-    serializer_class = WomanSerializer
+# class WomanViewSet(viewsets.ModelViewSet):
+# class WomanViewSet(viewsets.ReadOnlyModelViewSet):
+class WomanViewSet(mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                #    mixins.DestroyModelMixin,
+                   mixins.ListModelMixin,
+                   GenericViewSet
+                   ):
+   #  queryset =Woman.objects.all()
+    serializer_class=WomanSerializer
+    
+    def get_queryset(self):
+        pk = self.kwargs.get("pk")
+        if not pk:
+           return Woman.objects.all()[:3]
+        return Woman.objects.filter(pk=pk)
+    @action(methods=['get'],detail=True)
+    def category(self,request,pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats':cats.name})
+
+# class WomanAPIList(generics.ListCreateAPIView):
+#     queryset = Woman.objects.all()
+#     serializer_class = WomanSerializer
     
 
-class WomanAPIUpdate(generics.UpdateAPIView):
-    queryset = Woman.objects.all()
-    serializer_class = WomanSerializer
+# class WomanAPIUpdate(generics.UpdateAPIView):
+#     queryset = Woman.objects.all()
+#     serializer_class = WomanSerializer
 
-class WomanAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Woman.objects.all()
-    serializer_class = WomanSerializer
-
-# class WomanAPIView(APIView):
-    
-#     def get(self, request):
-#         w =Woman.objects.all()
-#         return Response({'posts':WomanSerializer(w, many=True).data})
-    
-#     def post(self, request):
-#         serializer = WomanSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         # post_new = Woman.objects.create(
-#         #     title = request.data['title'],
-#         #     content =request.data['content'],
-#         #     cat_id = request.data['cat_id']
-#         # )
-#         serializer.save()
-#         return Response({'post':serializer.data})
-    
-#     def put(self,request,*args,**kwargs):
-#         pk = kwargs.get("pk",None)
-#         if not pk:
-#             return Response({"error":"Method PUT not allowed"})
-#         try:
-#             instance =Woman.objects.get(pk=pk)
-#         except:
-#             return Response({"error":"Object doesn't exists"})
-#         serializer = WomanSerializer(data=request.data,instance=instance)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response({"post":serializer.data})
-    
-#     def delete(self,request,*args,**kwargs):
-#         pk=kwargs.get("pk",None)
-#         if not pk:
-#             return Response({"error":"Method DELETE not allowed"})
-#         return Response({"post":"delete post" + str(pk)})
-    
-# Create your views here.
-# class WomanAPIView(generics.ListAPIView):
+# class WomanAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
 #     queryset = Woman.objects.all()
 #     serializer_class = WomanSerializer
